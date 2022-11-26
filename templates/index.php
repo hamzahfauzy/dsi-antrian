2a88cf14c18da51785ae767091dd27200b08febc
hamzahfauzy <hamzahfauzy97@gmail.com>
2022-11-26 22:22:18 +0700

    fix index title

--- templates/index.php
+++ templates/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-	<title>Dinas Pendapatan Kabupaten Asahan</title>
+	<title>Badan Pendapatan Daerah Kabupaten Asahan</title>
 	<meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
 	<link rel="icon" href="<?=asset('assets/img/logo-bwhite.webp')?>" type="image/x-icon"/>
 
