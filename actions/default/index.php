c2ecdf6463e17e0a5bf5c0d20c4a22329192c4a2
hamzahfauzy <hamzahfauzy97@gmail.com>
2022-12-06 13:07:04 +0700

    today stats

--- actions/default/index.php
+++ actions/default/index.php
@@ -9,7 +9,9 @@ $data = [
     'pos' => $db->exists('pos'),
     'services' => $db->exists('services'),
     'queues_wait' => $db->exists('queues',['status'=>'menunggu']),
+    'queues_wait_today' => $db->exists('queues',['status'=>'menunggu','created_at'=>['LIKE','%'.date('Y-m-d').'%']]),
     'queues_called' => $db->exists('queues',['status'=>'selesai']),
+    'queues_called_today' => $db->exists('queues',['status'=>'selesai','created_at'=>['LIKE','%'.date('Y-m-d').'%']]),
 ];
 
 return compact('data');
