f673530af98598f4209d096f6eb1d01c9913a5ce
hamzahfauzy <hamzahfauzy97@gmail.com>
2022-12-06 13:08:55 +0700

    fix stats

--- actions/default/index.php
+++ actions/default/index.php
@@ -5,13 +5,19 @@ Page::set_title('Dashboard');
 $conn = conn();
 $db   = new Database($conn);
 
+$params = [];
+if(Session::get('pos'))
+{
+    $params = ['pos_id' => Session::get('pos')];
+}
+
 $data = [
     'pos' => $db->exists('pos'),
     'services' => $db->exists('services'),
-    'queues_wait' => $db->exists('queues',['status'=>'menunggu']),
-    'queues_wait_today' => $db->exists('queues',['status'=>'menunggu','created_at'=>['LIKE','%'.date('Y-m-d').'%']]),
-    'queues_called' => $db->exists('queues',['status'=>'selesai']),
-    'queues_called_today' => $db->exists('queues',['status'=>'selesai','created_at'=>['LIKE','%'.date('Y-m-d').'%']]),
+    'queues_wait' => $db->exists('queues',array_merge($params,['status'=>'menunggu'])),
+    'queues_wait_today' => $db->exists('queues',array_merge($params,['status'=>'menunggu','created_at'=>['LIKE','%'.date('Y-m-d').'%']])),
+    'queues_called' => $db->exists('queues',array_merge($params,['status'=>'selesai'])),
+    'queues_called_today' => $db->exists('queues',array_merge($params,['status'=>'selesai','created_at'=>['LIKE','%'.date('Y-m-d').'%']])),
 ];
 
 return compact('data');
